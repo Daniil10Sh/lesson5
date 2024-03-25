6e53bf243c918b3e7c23fc85bae59262b6bd4be5
Daniil10Sh <danik2022odessa@gmail.com>
2024-03-25 15:56:53 +0200

    add settings

---
+++ script.js
@@ -0,0 +1,72 @@
+// let result = 5+5+'5';
+// console.log(typeof result);
+
+// let email = 'danik2022odessa@gmail.com';
+// console.log(email.includes(@));
+// console.log(email.length);
+
+// let whose = 'My';
+// let name = 'name';
+// let is = 'is';
+// let fullname = 'My name is';
+// let name2 = '  Victor';
+// console.log(fullname + name2)
+
+// let userName = 'Дякуємо, Володимир!';
+// let payment = 'До сплати 500 гривень';
+// console.log(alert(userName + payment))
+
+// const monthes = document.querySelector('.monthes'); 
+// console.log (monthes); 
+ 
+// const whichmonth = document.querySelector('.whichmonth'); 
+// console.log (whichmonth); 
+ 
+//  const p = document.querySelector('.show-massedge'); 
+//  console.log (p); 
+ 
+//  function handlButtonclick() { 
+//     console.log ('hi'); 
+//     const year = monthes.value; 
+ 
+//     switch(year){  
+//         case "1": 
+//         case "2": 
+//             alert('winter'); 
+//         case "3": 
+//         case "4": 
+//         case "5": 
+//             alert('Spring'); 
+//         case "6": 
+//         case "7": 
+//         case "8": 
+//             alert('Summer'); 
+//         case "9": 
+//         case "10": 
+//         case "11": 
+//             alert('Autumn'); 
+//         case "12": 
+//             alert('Winter'); 
+//          break; 
+//          default: 
+//          alert('it is not a month ') 
+//          break; 
+//     } 
+// } 
+ 
+// whichmonth.addEventListener('click', handlButtonclick);
+
+// let num = 1;
+// while(num <= 10){
+//     console.log(num)
+//     num++;
+// }
+ 
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * 2);
+// }
+
+let arr = [2, 75, 97, 4, 5, 67, 57, 48, 9, 10]
+let max = Math.max(...arr);
+console.log(max);
